1463e2a2cf740d41b2317cd2151784e31d6f579c
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-01 10:57:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jpa-specification-example
+ 
---
+++ src/main/java/com/codelabs/JpaSpecificationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.codelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpecificationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpecificationExampleApplication.class, args);
+	}
+
+}
