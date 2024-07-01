3137ded1649d384c2b073b6a1086a4998fc29af2
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2024-07-01 11:09:13 +0530

    Create UserController.java

---
+++ src/main/java/com/codelabs/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.codelabs.controller;
+
+import com.codelabs.entity.User;
+import com.codelabs.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users/by-username")
+    public List<User> getUsersByUsername(@RequestParam String username) {
+        return userService.getUsersWithUsername(username);
+    }
+
+    @GetMapping("/users/by-email")
+    public List<User> getUsersByEmail(@RequestParam String email) {
+        return userService.getUsersWithEmail(email);
+    }
+
+    @GetMapping("/users/by-post-content")
+    public List<User> getUsersByPostContent(@RequestParam String content) {
+        return userService.getUsersWithPostContent(content);
+    }
+}
